321fe0c3ef4428a05a4be915fbc45fa609dbbaa3
Dante Alves <dantealves8@gmail.com>
2026-01-20 13:34:04 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		Seller seller = sellerDao.findById(2);
+		
 		System.out.println(seller);
 	}
 
